9e294162ed1f657e32be1722d97a3f3ea522c63b
mengsig <marcus.engsig@tii.ae>
2023-08-23 14:44:28 +0400

    pdfPlot

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This will be a continuous work in progress, but the following things are include
 	1. 2D lattice - random and uniform percolation with visualization and sliding bar for $\alpha$, along with a resetting button to the critical value for the system. This has been implemented using networkx, and displays the links.
 	2. 2D lattice - random and  uniform percolation with visualization (imshow) for extremely fast visualization (but without links). Implemented using sparse matrices and sparse graph routines for coloring purposes. As of now, I havent found a good solution for the coloring scheme.
 	3. 2D lattice - uniform percolation with temporal visualization of increasing and decreasing probability of removing links (here the probability is oscillating between [0,1]). The temporal visualization can be linear or based on a normal distribution around the critical value of the function (to slow down time around this). Note that the maximum recommended resolution is N = 2500 --> 6.250.000 million (nodes) or pixels on the screen. NOTE: here you must press CTRL-C afterwards to cancel the infinte loop that exists. 
+	4. 2D lattice - here we have the same as in point 3., however, in this part we also have added another figure that displays the probability density function of the connected compenents (cluster) sizes. 
 
 By Marcus Engsig
 22/08/2023.
---
+++ temporalPercolationDualDisplay.py
@@ -0,0 +1,128 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+from pylab import *
+from matplotlib.widgets import Slider
+from numba import prange, njit, jit
+#defining the update function
+def update(alpha):
+	#first we rewire according to the alpha rule
+	updatedAdj = probMatrix > (1-alpha)
+	#now we find the largest component
+	noComponent, componentIndex = sp.sparse.csgraph.connected_components(updatedAdj, connection = 'weak', directed = False)
+	componentIndex = np.reshape(componentIndex, (N,N))
+	ax.clear()
+	ax.imshow(componentIndex) #change colormap here... i couldn't find a better one.
+	#here we start plotting the distriubtion of component sizes
+	import time
+	t1 = time.time()
+	unique, counts = np.unique(componentIndex, return_counts = True)
+	t1 = time.time()
+	plotData = generate_histogram_data(componentIndex, counts)
+	newUnique, newCounts = np.unique(plotData, return_counts = True)
+	for i in range(newCounts.shape[0]):
+		newCounts[i] = newCounts[i]/newUnique[i]
+	newCounts = newCounts/newCounts.sum()
+	#creating the net plot bx
+	bx.clear()
+	#comment the two lines below if you don't want to lock the axis.
+	bx.set_xlim(0.5, 2*nodes)
+	bx.set_ylim(0.5/nodes, 2)
+	bx.set_xlabel('Cluster Size, L', fontsize = 22)
+	bx.set_ylabel('probability, p', fontsize = 22)
+	bx.loglog(newUnique, newCounts, 'kx')
+
+#defining the data extraction function for bx.
+@jit(parallel = True)
+def generate_histogram_data(componentIndex, counts):
+	dataStruct = np.zeros(componentIndex.shape)
+	for i in prange(componentIndex.shape[0]):
+		for j in range(componentIndex.shape[0]):
+			dataStruct[i,j] = counts[componentIndex[i,j]]
+	return dataStruct
+		
+#defining the stop function
+def reset(event):
+	plt.pause(1000)
+
+#defining the resume function
+def resume(event):
+	plt.gcf().canvas.stop_event_loop()
+
+#defining the number of nodes and grid size (N)
+N = 1000
+nodes = N**2
+#defining the number of iterations in the time steps (resolution)
+timeSteps = 250
+
+#generating the graph just for the adjacency matrix
+from networkx import grid_2d_graph, to_scipy_sparse_array, to_numpy_array
+import time
+t1 = time.time()
+G = grid_2d_graph(N,N)
+print(f'Time taken to generate networkx grid: {time.time() - t1} seconds')
+#generating the sparse uniform probability matrix for checking if links get removed or not.
+t1 = time.time()
+GAdj = to_scipy_sparse_array(G, format = 'coo').astype(np.float32)
+GAdj = sp.sparse.triu(GAdj, k=0)
+#extract the values of the sparse array
+dataOrig = GAdj.data
+data = GAdj.data
+#randomize the values of the sparse array and store them again
+data = data * np.random.uniform(0,1, data.shape[0])
+GAdj.data = data
+#generate the sparse csr_array for fast comparisons and components computation 
+probMatrix = sp.sparse.csr_array(GAdj)
+print(f'Time taken to generate the probability matrix: {time.time() - t1} seconds')
+
+#creating the initial graph
+fig, (ax, bx) =  plt.subplots(1,2)
+matplotlib.rcParams.update({'font.size':22})
+plt.rcParams.update({'font.size' : 22})
+bx.set_xlabel('Cluster Size, L')
+bx.set_ylabel('Probability, p')
+
+#initializing the drawing
+alpha = 0.5
+update(alpha)
+
+plt.subplots_adjust(bottom = 0.25)
+axAlpha = plt.axes([0.25, 0.10, 0.65, 0.03])
+
+#creating a slider.
+alphaSlider = Slider(axAlpha, 'Probability', 0, 1.0)
+alphaSlider.on_changed(update)
+
+#creating the button (for stopping)
+resetAlpha = plt.axes([0.00, 0.4, 0.10, 0.10])
+button = Button(resetAlpha, 'Pause')
+button.on_clicked(reset)
+#creating the button (for resuming)
+resetButton = plt.axes([0.00, 0.30, 0.10, 0.10])
+startButton = Button(resetButton, 'Resume')
+startButton.on_clicked(resume)
+
+x = np.linspace(0,timeSteps, timeSteps)
+#this line below is if you want non-linear time (ie. slow down as it gets to the critical point)
+wait = 0.25*np.e**((-(x-timeSteps/2)**2)/(0.15**2*timeSteps**2))
+#this line below is if you want linear time.
+wait = 0.001*np.ones(timeSteps)
+counter = 0
+while counter < timeSteps:
+	if counter == 0:
+		probMatrix.data = dataOrig*np.random.uniform(0,1,dataOrig.shape[0])
+	ax.clear()
+	alphaSlider.set_val(counter/timeSteps)
+	plt.show(block = False)
+	plt.pause(wait[counter])
+	counter +=1
+	if counter >= timeSteps - 1:
+		probMatrix.data = dataOrig*np.random.uniform(0,1,dataOrig.shape[0])
+		while counter >= 1:
+			ax.clear()
+			alphaSlider.set_val(counter/timeSteps)
+			plt.show(block = False)
+			plt.pause(wait[counter])
+			counter += -1
+plt.show()
+
